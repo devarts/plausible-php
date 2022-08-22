3fe6eceaafe6c3a21e5d1492c2148d6579808e07
Marko Ognjenovic <marko.ognjenovicgm@gmail.com>
2022-08-22 19:08:39 +0200

    Implement Stats API requests

--- src/PlausibleAPI.php
+++ src/PlausibleAPI.php
@@ -4,7 +4,7 @@ namespace Plausible;
 
 use GuzzleHttp\Client;
 
-abstract class PlausibleAPI
+class PlausibleAPI
 {
     protected Client $client;
 
@@ -16,5 +16,50 @@ abstract class PlausibleAPI
             'Authorization' => sprintf('Bearer %s', $token),
           ],
         ]);
+    }
+
+    public function getRealtimeVisitors(string $site_id): int
+    {
+        $response = $this->client->get('stats/realtime/visitors', [
+            'query' => [
+                'site_id' => $site_id,
+            ],
+        ]);
+
+        return (int) $response->getBody()->getContents();
+    }
+
+    public function getAggregate(string $site_id, array $payload = []): array
+    {
+        $response = $this->client->get('stats/aggregate', [
+            'query' => array_merge([
+                'site_id' => $site_id,
+            ], $payload),
+        ]);
+
+        return json_decode($response->getBody()->getContents(), true);
+    }
+
+    public function getTimeseries(string $site_id, array $payload = []): array
+    {
+        $response = $this->client->get('stats/timeseries', [
+            'query' => array_merge([
+                'site_id' => $site_id,
+            ], $payload),
+        ]);
+
+        return json_decode($response->getBody()->getContents(), true);
+    }
+
+    public function getBreakdown(string $site_id, string $property, array $payload = []): array
+    {
+        $response = $this->client->get('stats/breakdown', [
+            'query' => array_merge([
+                'site_id' => $site_id,
+                'property' => $property,
+            ], $payload),
+        ]);
+
+        return json_decode($response->getBody()->getContents(), true);
     }
 }
