6c3fce6b0e39aee91bf83c07cb2e3b3addfd7619
Marko Ognjenovic <marko.ognjenovicgm@gmail.com>
2022-09-03 15:14:15 +0200

    Implement Properties support class

---
+++ src/Support/Properties.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Plausible\Support;
+
+class Properties
+{
+    public const EVENT_NAME = 'event:name';
+    public const EVENT_PAGE = 'event:page';
+    public const VISIT_ENTRY_PAGE = 'visit:entry_page';
+    public const VISIT_EXIT_PAGE = 'visit:exit_page';
+    public const VISIT_SOURCE = 'visit:source';
+    public const VISIT_REFERRER = 'visit:referrer';
+    public const VISIT_UTM_MEDIUM = 'visit:utm_medium';
+    public const VISIT_UTM_SOURCE = 'visit:utm_source';
+    public const VISIT_UTM_CAMPAIGN = 'visit:utm_campaign';
+    public const VISIT_UTM_CONTENT = 'visit:utm_content';
+    public const VISIT_UTM_TERM = 'visit:utm_term';
+    public const VISIT_DEVICE = 'visit:device';
+    public const VISIT_BROWSER = 'visit:browser';
+    public const VISIT_BROWSER_VERSION = 'visit:browser_version';
+    public const VISIT_OS = 'visit:os';
+    public const VISIT_OS_VERSION = 'visit:os_version';
+    public const VISIT_COUNTRY = 'visit:country';
+    public const VISIT_REGION = 'visit:region';
+    public const VISIT_CITY = 'visit:city';
+}
