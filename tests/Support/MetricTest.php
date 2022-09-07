e5a63619cb91c5e95cade8b99ba3699f2fb5f701
Marko Ognjenovic <marko.ognjenovicgm@gmail.com>
2022-09-07 21:27:25 +0200

    Write MetricTest

--- tests/Support/MetricTest.php
+++ tests/Support/MetricTest.php
@@ -2,9 +2,45 @@
 
 namespace Plausible\Test\Support;
 
+use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
+use Plausible\Support\Metric;
 
 class MetricTest extends TestCase
 {
+    /**
+     * @test
+     */
+    public function it_should_stringify_metrics(): void
+    {
+        $metric = Metric::create()
+            ->add(Metric::VISITS)
+            ->add(Metric::VISIT_DURATION);
 
+        $this->assertEquals(
+            'visits,visit_duration',
+            $metric->toString()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_stringify_empty_metrics(): void
+    {
+        $metric = Metric::create();
+
+        $this->assertEquals('', $metric->toString());
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_throw_exception_when_adding_filter_for_unsupported_property(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Unsupported metric provided: `unsupported_metric`');
+
+        Metric::create()->add('unsupported_metric');
+    }
 }
